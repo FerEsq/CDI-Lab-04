91614a7d8f830f8dd2b64fe1541e8a28bc666d6c
AMontoyaQP <84055444+Montoya086@users.noreply.github.com>
2025-03-29 21:10:20 -0600

    logs

--- server/config/database.py
+++ server/config/database.py
@@ -2,6 +2,5 @@ from pymongo import MongoClient
 from flask import current_app
 
 def get_db():
-    print("here!!!", current_app.config['MONGODB_URI'])
     client = MongoClient(current_app.config['MONGODB_URI'])
     return client["file_system"]
